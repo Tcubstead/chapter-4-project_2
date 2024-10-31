38e2605d94b614c610c49f45461ab2436d08787a
Tcubstead <tec44912@email.vccs.edu>
2024-10-31 18:20:08 -0400

    created functions for the calculations

--- chapter 4 project_2.cpp
+++ chapter 4 project_2.cpp
@@ -7,18 +7,23 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const double PI = 3.14159
+double circleAreaFormula(double radius) {
+	return PI * radius * radius;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+double rectangleAreaFormula(double length, width) {
+	return length * width;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+double triangleAreaFormula(double base, height) {
+	return base * height * .5;
+}
+
+int main() {
+	int choice;
+	double radius, length, width, base, height;
+	do {
+		cout
+	}
+}
